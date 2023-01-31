858260a356661687cf56c5b759a75d15cda4d28b
Nova Gh <nghimire.dev@gmail.com>
2023-01-30 19:48:37 -0800

    Initial commit
    installed next, tailwind pkgs

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="main h-screen ">
+      <h1 className="section-title">Landing Page</h1>
+      <div className="flex "></div>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: {
+          light: "#2C3755",
+          DEFAULT: "#10141f",
+          dark: "#090B11",
+        },
+        brand: {
+          light: "#E36D68",
+          DEFAULT: "#d32d27",
+          dark: "#741915",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
