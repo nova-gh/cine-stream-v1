13f3445ad665769933f6a82e1bc3442e3ba5abfb
Nova Gh <nghimire.dev@gmail.com>
2023-02-10 23:53:32 -0800

    Added react-toastify

--- components/common/AuthProvider.tsx
+++ components/common/AuthProvider.tsx
@@ -1,13 +1,20 @@
 "use client";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type Props = {
   children: React.ReactNode;
   session: Session | null;
 };
 const AuthProvider = ({ children, session }: Props) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider session={session}>
+      <ToastContainer theme="dark" />
+      {children}
+    </SessionProvider>
+  );
 };
 
 export default AuthProvider;
